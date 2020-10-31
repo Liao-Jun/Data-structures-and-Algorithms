337fd60c4c348000cc78102802962d57031ea85e
liaojun <2488731101@qq.com>
2020-10-31 21:28:48 +0800

    main

--- UVA/Uva_11988.cpp
+++ UVA/Uva_11988.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 #include <string>
+#include <list>
 using namespace std;
 
 int main(){
-    
+    string s;
+    while(cin >> s){
+        list<char> l;
+        list<char>::iterator it;
+        it = l.begin();
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='['){
+                it = l.begin();
+            }else if(s[i]==']'){
+                it = l.end();
+            }else{
+                it = l.insert(it, s[i]);
+                it++;
+            }
+        }
+        for(auto i : l){
+            cout << i;
+        }
+        cout << endl;
+        l.clear();
+    }
+
+    return 0;
 }
+
+//https://vjudge.net/problem/UVA-11988
