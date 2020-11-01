24faebd03bf5479ec87c5cd7b3903b9b0689dd6e
liaojun <2488731101@qq.com>
2020-11-01 13:05:36 +0800

    main

--- UVA/Uva_12657.cpp
+++ UVA/Uva_12657.cpp
@@ -1,30 +1,60 @@
 #include <iostream>
 #include <list>
-#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main(){
-    ios::sync_with_stdio;
-    int t;
-    cin >> t;
-    while(t--){
-        int n,m;
-        cin >> n >> m;
-        list<int> l;
-        list<int>::iterator it;
-        it = l.begin();
-        for(int j=1;j<=n;j++){
-            l.insert(it,j);
-            it++;
+    int n,m;
+    int k = 1;
+    while(scanf("%d%d",&n,&m)==2){
+        list<int> l(n);
+        vector<list<int>::iterator> p(n+1);
+        int i=1;
+        for(auto q=l.begin();q!=l.end();q++,i++){
+            *q = i;
+            p[i] = q;
         }
+        int flag,a,b;
+        int re = -1;
         for(int i=0;i<m;i++){
-            string s;
-            getline(cin,s);
-            if(s[0]=='1'){
-                
+            cin >> flag;
+            if(flag==4){
+                re = -1*re;
+            }else if(flag==3){
+                scanf("%d%d",&a,&b);
+                swap(*p[a], *p[b]);
+                swap(p[a], p[b]);
+            }else{
+                scanf("%d%d",&a,&b);
+                if(re==1){
+                    flag = 3-flag;
+                }
+                l.erase(p[a]);
+                auto y = p[b];
+                if(flag==2){
+                    y++;
+                }
+                p[a] = l.insert(y, a);
             }
+        }
+        long long int sum = 0;
+        if(re==-1){
+            for(auto it=l.begin(); it!=l.end();it++,it++){
+                sum += *it;
+            }
+            cout << "Case " << k++ << ": ";
+            cout << sum << endl;
+        }else{
+            for(list<int>::reverse_iterator it=l.rbegin(); it!=l.rend();++it,++it){
+                sum += *it;
+            }
+            cout << "Case " << k++ << ": ";
+            cout << sum << endl;
         }
     }
 
     return 0;
 }
+
+//https://vjudge.net/problem/UVA-12657
