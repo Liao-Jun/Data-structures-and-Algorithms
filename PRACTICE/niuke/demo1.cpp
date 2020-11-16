e6827074919d09798811e95c8b1cf278e46e88d0
liaojun <2488731101@qq.com>
2020-11-16 10:47:50 +0800

    main

--- PRACTICE/niuke/demo1.cpp
+++ PRACTICE/niuke/demo1.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main(){
-    
+    bool x = true;
+    cout << x << "&1 " << (x&1) << endl;
+    cout << x << "&0 " << (x&0) << endl;
+    cout << x << "|1 " << (x|1) << endl;
+    cout << x << "|0 " << (x|0) << endl;
+    cout << x << "^1 " << (x^1) << endl;
+    cout << x << "^0 " << (x^0) << endl;
+    x = false;
+    cout << x << "&1 " << (x&1) << endl;
+    cout << x << "&0 " << (x&0) << endl;
+    cout << x << "|1 " << (x|1) << endl;
+    cout << x << "|0 " << (x|0) << endl;
+    cout << x << "^1 " << (x^1) << endl;
+    cout << x << "^0 " << (x^0) << endl;
+    return 0;
 }
