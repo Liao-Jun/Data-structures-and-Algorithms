1a04e07ed91cf5ab5e77fba52ee667723bcdcfc1
liaojun <2488731101@qq.com>
2020-12-11 11:20:13 +0800

    main

--- Board/最短路径Dijkstra+优先队列.cpp
+++ Board/最短路径Dijkstra+优先队列.cpp
@@ -9,69 +9,77 @@
 // 接下来T行:每一行都用三个空格分隔的整数描述一个轨迹。前两个整数是路线经过的公交站台。第三个整数是路径的长度，范围为1到100。
 // Output:
 // 一个整数，表示大佬找到他女朋友的最小距离。
-// Sample Input
+// Sample Input:
 // 5 5
 // 1 2 20
 // 2 3 30
 // 3 4 20
 // 4 5 20
 // 1 5 100
-// Sample Output
+// Sample Output:
 // 90
 #include <iostream>
 #include <cstring>
 #include <algorithm>
+#include <vector>
+#include <queue>
 using namespace std;
 
 const int INF = 0x3f3f3f3f;
-int cost[1010][1010];
-int used[1010];
-int dist[1010];
+
+struct node{
+    int v,cost;
+    node(int _v,int _cost):v(_v),cost(_cost){};
+};
+
+struct Node{
+    int point,dist;
+    Node(int _point, int _dist):point(_point),dist(_dist){};
+    bool operator<(const Node &a)const{
+        return dist>a.dist;
+    }
+};
+
 int V;
+int dist[1010];
+int used[1010];
+vector<node> e[1010];
 
 void Dijkstra(){
-    int k,min;
-    for(int i=1;i<=V;i++){
-        dist[i] = cost[1][i];
-        used[i] = 0;
-    }
-    for(int i=1;i<=V;i++){
-        k = 0;
-        min = INF;
-        for(int j=1;j<=V;j++){
-            if(used[j]==0&&dist[j]<min){
-                min = dist[j];
-                k = j;
+    priority_queue<Node> q;
+    dist[1] = 0;
+    q.push(Node(1,0));
+    while(!q.empty()){
+        Node temp = q.top();
+        q.pop();
+        int u = temp.point;
+        if(used[u]==1){
+            continue;
+        }
+        used[u] = 1;
+        for(int i=0;i<e[u].size();i++){
+            int v = e[u][i].v;
+            int cost = e[u][i].cost;
+            if(dist[u]+cost<dist[v]&&used[v]==0){
+                dist[v] = dist[u]+cost;
+                q.push(Node(v,dist[v]));
             }
         }
-        used[k] = 1;
-        for(int j=1;j<=V;j++){
-            if(used[j]==0&&dist[k]+cost[k][j]<dist[j]){
-                dist[j] = dist[k]+cost[k][j];
-            }
-        }
     }
-    return;
+
 }
 
 int main(){
     int t,n;
     cin >> t >> n;
-    int x,y,z;
-    memset(used,0,sizeof(used));
-    memset(dist,INF,sizeof(dist));
     V = n;
-    for(int i=1;i<=V;i++){
-        for(int j=1;j<=V;j++){
-            cost[i][j] = INF;
-        }
-    }
+    memset(dist,INF,sizeof(dist));
+    memset(used,0,sizeof(used));
+    int x,y,z;
     for(int i=1;i<=t;i++){
         cin >> x >> y >> z;
-        if(z<cost[x][y]){
-            cost[x][y] = z;
-            cost[y][x] = z;
-        }
+        e[x].push_back(node(y,z));
+        e[y].push_back(node(x,z));
     }
     Dijkstra();
     cout << dist[V] << endl;
