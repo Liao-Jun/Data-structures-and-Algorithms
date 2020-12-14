2b060776e03417e7db2017b0e854331e368816b0
liaojun <2488731101@qq.com>
2020-12-14 11:28:48 +0800

    main

--- POJ/Poj_3255.cpp
+++ POJ/Poj_3255.cpp
@@ -1,70 +1,66 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <string.h>
-#include <stdio.h>
+#include <cstring>
 #include <algorithm>
+#include <functional>
 using namespace std;
-const int maxn=5005;
-const int inf=1e9;
-int N,R;
-struct edge
-{
+
+const int INF = 0x3f3f3f3f;
+const int M = 5e3+10;
+struct edge{
     int to,cost;
 };
-typedef pair<int ,int> P ;///first 是从1到second的最短路 second 是路口标号
-vector<edge>G[maxn];///邻接表
-int dist[maxn];///最短路
-int dist2[maxn];///次短路
-void solve()
-{
-    priority_queue<P ,vector<P>,greater<P> >que;
-    fill(dist,dist+N,inf);
-    fill(dist2,dist2+N,inf);
-    dist[0]=0;
-    //dist2[0]=0;
-    que.push(P(0,0));
-    while(!que.empty())
-    {
-        P p=que.top();///优先队列 ，用.top
-        que.pop();
-        int v=p.second ,d=p.first;
-        if(dist2[v]<d)continue;
-        for(int i=0; i<G[v].size(); i++)
-        {
-            edge e=G[v][i];
-            int d2=d+e.cost;
-            if(dist[e.to]>d2)
-            {
-                swap(dist[e.to],d2);
-                que.push(P(dist[e.to],e.to));
+vector<edge> ed[M];
+int dist[M];
+int dist1[M];
+int V;
+typedef pair<int,int> P;
+
+void Dijkstra(){
+    priority_queue<P,vector<P>,greater<P>> q;
+    dist[0] = 0;
+    q.push(P(0,0));
+    while(!q.empty()){
+        P p = q.top();
+        q.pop();
+        int v = p.second;
+        int d = p.first;
+        if(dist1[v]<d) continue;
+        for(int i=0;i<ed[v].size();i++){
+            edge e = ed[v][i];
+            int d1 = d + e.cost;
+            if(dist[e.to]>d1){
+                swap(dist[e.to],d1);
+                q.push(P(dist[e.to],e.to));
             }
-            if(dist2[e.to]>d2&&dist[e.to]<d2)
-            {
-                dist2[e.to]=d2;
-                que.push(P(dist[e.to],e.to));
-                que.push(P(dist2[e.to],e.to));
+            if(dist1[e.to]>d1&&dist[e.to]<d1){
+                dist1[e.to] = d1;
+                q.push(P(dist1[e.to],e.to));
             }
         }
     }
-    printf("%d\n",dist2[N-1]);
 }
-int main()
-{
+
+int main(){
+    int n,r;
+    scanf("%d%d",&n,&r);
+    memset(dist,INF,sizeof(dist));
+    memset(dist1,INF,sizeof(dist1));
+    edge e;
     int from;
-    while(cin>>N>>R)
-    {
-        edge now;
-        for(int i=0;i<R;i++)
-        {
-            cin>>from>>now.to>>now.cost;
-            from--;
-            now.to--;///标号从  0-N-1！！
-            G[from].push_back(now);
-            swap(now.to,from);
-            G[from].push_back(now);
-        }
-        solve();
+    int V = n;
+    for(int i=0;i<r;i++){
+        cin >> from >> e.to >> e.cost;
+        from--;
+        e.to--;
+        ed[from].push_back(e);
+        swap(e.to,from);
+        ed[from].push_back(e);
     }
+    Dijkstra();
+    cout << dist1[V-1] << endl;
     return 0;
 }
+
+//https://vjudge.net/problem/POJ-3255
