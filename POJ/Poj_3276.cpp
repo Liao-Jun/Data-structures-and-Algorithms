96d7a8ab93495a1625b3af6d53cf9b2040197f8f
liaojun <2488731101@qq.com>
2020-12-21 15:30:59 +0800

    main

--- POJ/Poj_3276.cpp
+++ POJ/Poj_3276.cpp
@@ -11,9 +11,9 @@
 #include <map>
 #include <stack>
 #include <set>
-#pragma GCC optimize("O3")
-#pragma G++ optimize("O3")
-#define DEBUG
+// #pragma GCC optimize("O3")
+// #pragma G++ optimize("O3")
+// #define DEBUG
 using namespace std;
 
 typedef long long ll;
@@ -22,18 +22,19 @@ typedef pair<int,int> P;
 const int INF = 0x3f3f3f3f;
 const int Mod = 1e9+7;
 const int EXP = 1e-8;
-inline ll gcd(ll x, ll y){if(!y) return x;return gcd(y,y%x);}
+// inline ll gcd(ll x, ll y){if(!y) return x;return gcd(y,y%x);}
+inline void debug(){printf("@@\n");}
 
 int main()
 {
-   ios::sync_with_stdio(false);
-   cin.tie(0);
-   cout.tie(0);
+//    ios::sync_with_stdio(false);
+//    cin.tie(0);
+//    cout.tie(0);
    #ifdef DEBUG
        freopen("C://Users//24887//Data-structures-and-Algorithms//input.in","r",stdin);
        freopen("C://Users//24887//Data-structures-and-Algorithms//output.out","w",stdout);
    #endif
-   printf("hello");
+   
 
    return 0;
 }
