6256a7bc3b81523e9d7e83fb48e598a3ca09d848
liaojun <2488731101@qq.com>
2020-12-26 23:45:35 +0800

    main

--- Codeforces/691div2/testC.cpp
+++ Codeforces/691div2/testC.cpp
@@ -1,36 +1,68 @@
 #include <iostream>
 #include <cstdio>
+#include <fstream>
 #include <algorithm>
+#include <cmath>
+#include <deque>
+#include <vector>
+#include <queue>
+#include <string>
+#include <cstring>
+#include <map>
+#include <stack>
+#include <set>
+#pragma GCC optimize("O3")
+#pragma G++ optimize("O3")
+#define endl '\n'
+#define PI acos(-1)
+// #define DEBUG
 using namespace std;
 
-const int M = 2e5+10;
 typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> P;
+const int INF = 0x3f3f3f3f;
+const int Mod = 1e9+7;
+const int EXP = 1e-8;
+inline ll gcd(ll x, ll y){if(y==0) return x;return gcd(y,x%y);}//x>y
+inline void debug(){printf("@@\n");}
+const int M = 2e5+10;
+int n,m;
 ll a[M],b[M];
 
-ll gcd(ll a, ll b){
-    if(b==0) return a;
-    gcd(b,a%b);
-}
-
-int main(){
-    int n,m;
-    scanf("%d%d",&n,&m);
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    #ifdef DEBUG
+        freopen("C://Users//24887//Data-structures-and-Algorithms//input.in","r",stdin);
+        freopen("C://Users//24887//Data-structures-and-Algorithms//output.out","w",stdout);
+    #endif
+    cin >> n >> m;
     for(int i=0;i<n;i++){
-        scanf("%lld",&a[i]);
+        cin >> a[i];
     }
     for(int i=0;i<m;i++){
-        scanf("%lld",&b[i]);
+        cin >> b[i];
     }
-    ll temp = a[0];
-    sort(a,a+n);
-    for(int i=1;i<n-1;i++){
-        temp = gcd(temp,a[i]);
+    ll x = gcd(abs(a[1]-a[0]),abs(a[2]-a[1]));
+    for(int i=3;i<n;i++){
+        x = gcd(x,abs(a[i]-a[i-1]));
     }
-    ll c;
-    for(int i=0;i<m;i++){
-        c = gcd(temp+b[i],a[n-1]+b[i]);
-        printf("%lld\n",c);
+    if(n==1){
+        cout << x+b[0];
+        for(int i=1;i<m;i++){
+            cout << ' ' << x+b[i];
+        }
     }
+    else{
+        cout << gcd(x,a[0]+b[0]);
+        for(int i=1;i<m;i++){
+            cout << ' ' << gcd(x,a[0]+b[i]);
+        }
+    }
+    cout << endl;
 
     return 0;
 }
--- contest/浙大城市学院/testD.cpp
+++ contest/浙大城市学院/testD.cpp
@@ -26,28 +26,7 @@ const int Mod = 1e9+7;
 const int EXP = 1e-8;
 // inline ll gcd(ll x, ll y){if(!y) return x;return gcd(y,y%x);}
 inline void debug(){printf("@@\n");}
-int Judge[100];
-int a[10];
-int b[10];
-int sum;
-
-bool check(int x){
-    memset(b,0,sizeof(b));
-    int y[3];
-    int X = x;
-    int c = 0;
-    while(X>9){
-        y[c++] = (X%10);
-        X /= 10;
-        b[y[c-1]] ++;
-    }
-    y[c++] = (X);
-    b[y[c-1]] ++;
-    for(int i=0;i<10;i++){
-        if(a[i]<b[i]) return false;
-    }
-    return true;
-}
+int f[100];
 
 int main()
 {
@@ -60,18 +39,38 @@ int main()
     #endif
     int cnt = 0;
     for(int i=0;i<1000;i++){
-        cin >> Judge[cnt++];
+        if(i%8==0){
+            f[cnt++] = i;
+        }
     }
     int t;
     cin >> t;
     while(t--){
+        int a[10];
+        int sum = 0;
         for(int i=0;i<10;i++){
             cin >> a[i];
+            sum += a[i];
         }
-        int flag = 0;
-        for(int i=0;i<cnt;i++){
-            if(check(Judge[i])){
-
+        if(sum==0) cout << -1 << endl;
+        else if(sum==1){
+            if(a[8]==1) cout << 8 << endl;
+        }else if(sum==2){
+            int num = 0;
+            if(a[8]==2) cout << 88 << endl;
+            else{
+                for(int i=9;i>=0;i--){
+                    if(a[i]==1) num = num*10+i;
+                }
+                if(num%8==0) cout << num << endl;
+                else{
+                    num = 0;
+                    for(int i=0;i<10;i++){
+                        if(a[i]==1) num = num*10+i;
+                    }
+                    if(num%8==0) cout << num << endl;
+                    else cout << -1 << endl;
+                }
             }
         }
     }
