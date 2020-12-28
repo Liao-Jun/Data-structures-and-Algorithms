0caac4ad5ee3d534ba610c00b17ca217fdf6e291
liaojun <2488731101@qq.com>
2020-12-28 14:14:29 +0800

    main

--- PRACTICE/niuke/jishoudaxuexinxing/testC.cpp
+++ PRACTICE/niuke/jishoudaxuexinxing/testC.cpp
@@ -26,10 +26,11 @@ const int Mod = 1e9+7;
 const int EXP = 1e-8;
 // inline ll gcd(ll x, ll y){if(y==0) return x;return gcd(y,x%y);}//x>y
 inline void debug(){printf("@@\n");}
-const int M = 105;
-string s[M];
-int dic[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
-int used[105][105];
+int n,m;
+bool check(int x, int y){
+    if(x>=0&&x<n&&y>=0&&y<m) return true;
+    else return false;
+}
 
 int main()
 {
@@ -40,38 +41,29 @@ int main()
         freopen("C://Users//24887//Data-structures-and-Algorithms//input.in","r",stdin);
         freopen("C://Users//24887//Data-structures-and-Algorithms//output.out","w",stdout);
     #endif
-    int n,m;
-    while(~scanf("%d%d",&n,&m)){
+    string s[105];
+    while(cin >> n >> m){
         for(int i=0;i<n;i++){
             cin >> s[i];
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                used[i][j] = 0;
-                cout << used[i][j] << ' ';
-            }
-            cout << endl;
-        }
-        // debug();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(s[i][j]=='#'){
-                    for(int k=0;k<4;k++){
-                        int I = i+dic[k][0];
-                        int J = j+dic[k][1];
-                        if(s[I][J]=='*'&&I>=0&&I<n&&J>=0&&J<m){
-                            used[I][J]++;
-                            // cout << I <<' ' << J << endl;
-                        }
-                    }
-                }
-            }
         }
         vector<P> v;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(used[i][j]>1){
-                    v.push_back(make_pair(i+1,j+1));
+                int sum = 0;
+                if(s[i][j]=='*'){
+                    if(check(i-1,j)&&s[i-1][j]=='#'){
+                        sum++;
+                    }
+                    if(check(i+1,j)&&s[i+1][j]=='#'){
+                        sum++;
+                    }
+                    if(check(i,j-1)&&s[i][j-1]=='#'){
+                        sum++;
+                    }
+                    if(check(i,j+1)&&s[i][j+1]=='#'){
+                        sum++;
+                    }
+                    if(sum>1) v.push_back(make_pair(i+1,j+1));
                 }
             }
         }
