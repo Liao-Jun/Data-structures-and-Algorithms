79a7332347265d9cc58756f09859cc7382c743d7
liaojun <2488731101@qq.com>
2021-01-20 14:57:00 +0800

    main

--- Aizu/Aizu_0033.cpp
+++ Aizu/Aizu_0033.cpp
@@ -26,20 +26,21 @@ const int Mod = 1e9+7;
 const int EXP = 1e-8;
 // inline ll gcd(ll x, ll y){if(y==0) return x;return gcd(y,x%y);}//x>y
 inline void debug(){printf("@@\n");}
-int dic[][2] = {{1,0},{-1,0},{0,-1},{0,1}};
-int h,w;
-char s[105][105];
-int used[105][105];
-void dfs(int x, int y,int ch){
-    for(int i=0;i<4;i++){
-        int nx = x+dic[i][0];
-        int ny = y+dic[i][1];
-        if(nx>=0&&nx<h&&ny>=0&&ny<w&&used[nx][ny]==0){
-            if(s[nx][ny]==ch){
-                used[nx][ny] = 1;
-                dfs(nx,ny,ch);
-            }
-        }
+int s[15],L[10],R[10];
+int flag;
+void dfs(int x, int l, int r){
+    if(x>=10){
+        flag = 1;
+        return;
+    }
+    if(flag) return;
+    if(s[x]>L[l-1]){
+        L[l] = s[x];
+        dfs(x+1,l+1,r);
+    }
+    if(s[x]>R[r-1]){
+        R[r] = s[x];
+        dfs(x+1,l,r+1);
     }
 }
 
@@ -52,26 +53,21 @@ int main()
         freopen("C://Users//24887//Data-structures-and-Algorithms//input.in","r",stdin);
         freopen("C://Users//24887//Data-structures-and-Algorithms//output.out","w",stdout);
     #endif
-    while(cin>>h>>w&&!(h==0&&w==0)){
-        for(int i=0;i<h;i++){
-            for(int j=0;j<w;j++){
-                cin >> s[i][j];
-            }
+    int t;
+    cin >> t;
+    while(t--){
+        for(int i=0;i<10;i++){
+            cin >> s[i];
         }
-        memset(used,0,sizeof(used));
-        int cnt = 0;
-        for(int i=0;i<h;i++){
-            for(int j=0;j<w;j++){
-                if(used[i][j]==0){
-                    dfs(i,j,s[i][j]);
-                    cnt++;
-                }
-            }
-        }
-        cout << cnt << endl;
+        flag = 0;
+        L[0] = 0;
+        R[0] = 0;
+        dfs(0,1,1);
+        if(flag) cout << "YES\n";
+        else cout << "NO\n";
     }
 
     return 0;
 }
 
-//https://vjudge.net/problem/Aizu-0118
+//https://vjudge.net/problem/Aizu-0033
