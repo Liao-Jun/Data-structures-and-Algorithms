894449b0c640f3a8daa29d430fc3381c4ae1f6b8
liaojun <2488731101@qq.com>
2021-02-26 23:27:11 +0800

    main

---
+++ PRACTICE/牛客/练习赛/lianxi77/test-C.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <algorithm>
+#include <cmath>
+#include <deque>
+#include <vector>
+#include <queue>
+#include <string>
+#include <cstring>
+#include <map>
+#include <stack>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <time.h>
+#pragma GCC optimize("O3")
+#pragma G++ optimize("O3")
+// #define endl '\n'
+#define PI acos(-1)
+// #define DEBUG
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> P;
+const int INF = 0x3f3f3f3f;
+const int Mod = 1e9+7;
+const int EXP = 1e-8;
+// inline ll gcd(ll x, ll y){if(y==0) return x;return gcd(y,x%y);}//x>y
+inline void debug(){printf("@@\n");}
+ll g(int n){
+    ll sum = 0;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            if(i%j==0) sum += j;
+        }
+    }
+    return sum;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    #ifdef DEBUG
+        freopen("C://Users//24887//Data-structures-and-Algorithms//input.in","r",stdin);
+        freopen("C://Users//24887//Data-structures-and-Algorithms//output.out","w",stdout);
+    #endif
+    ll n;
+    cin >> n;
+    cout << g(g(n)) << endl;
+
+    return 0;
+}
