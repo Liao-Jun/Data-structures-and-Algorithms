9a907e0ee3a47985903b8209d4573f42dc983362
liaojun <2488731101@qq.com>
2021-05-28 12:50:51 +0800

    main

---
+++ 蓝桥杯/2017决赛/数组操作.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+using ll = long long;
+const int maxn = 1e5+10;
+
+ll a[maxn], sum[maxn];
+
+int main() {
+
+
+    return 0;
+}
