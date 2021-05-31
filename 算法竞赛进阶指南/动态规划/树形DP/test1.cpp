24ab1cddccfc6f6b2b70364f2e51002ad777a47e
liaojun <2488731101@qq.com>
2021-05-31 21:22:26 +0800

    main

--- 算法竞赛进阶指南/动态规划/树形DP/test1.cpp
+++ 算法竞赛进阶指南/动态规划/树形DP/test1.cpp
@@ -1,1 +1,36 @@
 //没有上司的舞会
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+const int maxn = 6010;
+
+int n, root, h[maxn], f[maxn], dp[2][maxn], ind[maxn], used[maxn];
+
+void dfs(int x) {
+    if (!x) return;
+    used[x] = 1;
+    root = x;
+    dp[0][f[x]] += max(dp[1][x] + h[x], dp[0][x]);
+    dp[1][f[x]] += dp[0][x];
+    ind[f[x]] --;
+    if (!ind[f[x]]) dfs(f[x]);
+}
+
+int main() {
+    cin >> n;
+    for (int i = 1; i <= n; i ++ ) cin >> h[i];
+    int a, b;
+    for (int i = 1; i < n; i ++ ) {
+        cin >> a >> b;
+        f[a] = b;
+        ind[b] ++;
+    }
+    for (int i = 1; i <= n; i ++ )
+        if (!used[i] && !ind[i])
+            dfs(i);
+    cout << max(dp[0][root], dp[1][root] + h[root]) << endl;
+
+    return 0;
+}
---
+++ 算法竞赛进阶指南/动态规划/树形DP/test2.cpp
@@ -0,0 +1,15 @@
+//选课
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    int i = 1;
+    int j = 0;
+    int x = ~i;
+    cout << ~i << ' ' << ~j << endl;
+
+    return 0;
+}
