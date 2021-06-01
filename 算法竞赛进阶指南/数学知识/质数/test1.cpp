f33d904a41444ad3e4bbd3b7badbdd956f30bc8c
liaojun <2488731101@qq.com>
2021-06-01 16:41:20 +0800

    main

---
+++ 算法竞赛进阶指南/数学知识/质数/test1.cpp
@@ -0,0 +1,24 @@
+//质数距离
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+using ll = long long;
+const int maxn = 1e6+10;
+
+bool st[maxn];
+
+void get_prime() {
+    cnt = 0;
+    for (int i = 2; i <= 5e5; i ++ ) {
+        
+    }
+}
+
+
+int main() {
+
+
+    return 0;
+}
