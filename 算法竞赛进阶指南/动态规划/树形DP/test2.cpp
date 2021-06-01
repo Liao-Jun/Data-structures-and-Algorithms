8eb47fd2e2bf54e71875a3177e856b265cf26190
liaojun <2488731101@qq.com>
2021-06-01 14:46:19 +0800

    main

--- 算法竞赛进阶指南/动态规划/树形DP/test2.cpp
+++ 算法竞赛进阶指南/动态规划/树形DP/test2.cpp
@@ -2,14 +2,43 @@
 #include <iostream>
 #include <algorithm>
 #include <cstring>
+#include <vector>
 
 using namespace std;
+const int maxn = 310;
+
+int n, m;
+vector<int> v[310];
+int dp[310][310];
+int w[310];
+
+void dfs(int u) {
+    for (int i = 0; i < v[u].size(); i ++ ) {
+        int x = v[u][i];
+        dfs(x);
+        for (int j = m; j >= 0; j -- ) {
+            for (int k = 0; k <= j; k ++ ) {
+                dp[u][j] = max(dp[u][j], dp[u][j - k] + dp[x][k]);
+            }
+        }
+    }
+    if (u != 0) {
+        for (int i = m; i >= 1; i -- ) {
+            dp[u][i] = dp[u][i - 1] + w[u];
+        }
+    }
+}
 
 int main() {
-    int i = 1;
-    int j = 0;
-    int x = ~i;
-    cout << ~i << ' ' << ~j << endl;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i ++ ) {
+        int x, y;
+        cin >> x >> y;
+        v[x].push_back(i);
+        w[i] = y;
+    }
+    dfs(0);
+    cout << dp[0][m] << endl;
 
     return 0;
 }
